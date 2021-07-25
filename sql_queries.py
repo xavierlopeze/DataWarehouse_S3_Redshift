450807805d9941c826bf94f94c71b7fab1574519
Xavier López <xavier.lopeze@gmail.com>
2021-07-25 17:44:32 +0200

    create tables and drop

--- sql_queries.py
+++ sql_queries.py
@@ -7,35 +7,110 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create = ("""
+    CREATE TABLE staging_events (
+        artist VARCHAR,
+        auth VARCHAR,
+        first_name VARCHAR,
+        gender CHAR,
+        session_item INT,
+        last_name VARCHAR,
+        length FLOAT,
+        level VARCHAR,
+        location VARCHAR,
+        method VARCHAR,
+        page VARCHAR,
+        registration BIGINT,
+        session_id INT,
+        song VARCHAR ,
+        status INT,
+        ts BIGINT,
+        user_agent VARCHAR,
+        user_id INT
+    );
 """)
 
+
 staging_songs_table_create = ("""
+    CREATE TABLE staging_songs (
+        artist_id VARCHAR,
+        artist_location VARCHAR,
+        artist_latitude FLOAT,
+        artist_longitude FLOAT,
+        artist_name VARCHAR,
+        duration FLOAT,
+        num_songs INT,
+        song_id VARCHAR,
+        title VARCHAR,
+        year INT
+    );
 """)
 
 songplay_table_create = ("""
+    CREATE TABLE songplays (
+        songplay_id INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+        start_time TIMESTAMP NOT NULL,
+        user_id VARCHAR NOT NULL,
+        level VARCHAR NOT NULL,
+        song_id VARCHAR NOT NULL,
+        artist_id VARCHAR NOT NULL,
+        session_id INT,
+        location VARCHAR,
+        user_agent VARCHAR
+    );
 """)
 
 user_table_create = ("""
+    CREATE TABLE users (
+        user_id VARCHAR NOT NULL PRIMARY KEY,
+        first_name VARCHAR NOT NULL,
+        last_name VARCHAR NOT NULL,
+        gender CHAR,
+        level VARCHAR NOT NULL
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE songs (
+        song_id VARCHAR NOT NULL PRIMARY KEY,
+        title VARCHAR NOT NULL,
+        artist_id VARCHAR NOT NULL,
+        year INT,
+        duration INT
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE artists (
+        artist_id VARCHAR NOT NULL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        location VARCHAR,
+        latitude FLOAT,
+        longitude FLOAT
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE time (
+        start_time TIMESTAMP NOT NULL PRIMARY KEY,
+        hour INT NOT NULL,
+        day INT NOT NULL,
+        week INT NOT NULL,
+        month INT NOT NULL,
+        year INT NOT NULL,
+        weekday INT NOT NULL
+    );
 """)
 
 # STAGING TABLES
